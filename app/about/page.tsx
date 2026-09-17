49f7bfa46eb77fde72cae068d768cc7d381cff02
Mohammad Esmaeilpour <105607844+mohamad-es@users.noreply.github.com>
2026-09-17 14:38:26 +0330

    feat: add about route

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/layout/header";
+import { AboutPage } from "@/components/about/about-page";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <AboutPage />
+    </>
+  );
+}
