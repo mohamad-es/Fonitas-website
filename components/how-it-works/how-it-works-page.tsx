e7972b7f72ec4fe74c78fa178796435c6e0954e8
Mohammad Esmaeilpour <105607844+mohamad-es@users.noreply.github.com>
2026-09-20 22:24:22 +0330

    chore: clean unused icons and optimize images

--- components/how-it-works/how-it-works-page.tsx
+++ components/how-it-works/how-it-works-page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ArrowLeft, ArrowRight, ArrowUpRight } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 
 import Image from "next/image";
 import Link from "next/link";
